a87b162c3eb24c2dbf71a62ef09e6b2f3a857d36
FDSAP-RALF VINCENT <ralfvincentbataller@gmail.com>
2023-05-15 10:09:36 +0800

    update added

--- lib/scr/screens/loginpage.dart
+++ lib/scr/screens/loginpage.dart
@@ -1,69 +1,119 @@
 import 'package:flutter/material.dart';
 import '../screens/registrationpage.dart';
 import '../screens/forgotpassword.dart';
+import '../screens/home.dart';
 
-class loginpage extends StatelessWidget {
+
+
+class loginpage extends StatefulWidget {
   const loginpage({Key? key}) : super(key: key);
+
+  @override
+  _loginpageState createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _email;
+  String? _password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.people),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!isValidEmail(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value;
+                },
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.key),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value;
+                },
               ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Perform login logic here
-              },
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 16.0),
-            GestureDetector(
-              onTap: () {
-                // Navigate to the password reset page or show a dialog
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => forgotpassword()),
-                );
-              },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // Perform login logic here using _email and _password
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => home()),
+                    );
+                  }
+                },
+                child: const Text('Login'),
+              ),
+              const SizedBox(height: 10.0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => forgotpassword()),
+                  );
+                },
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => registrationpage()),
-                );
-              },
-              child: const Text('Register'),
-            ),
-          ],
+              const SizedBox(height: 5.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => registrationpage()),
+                  );
+                },
+                child: const Text('Register'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
+  bool isValidEmail(String email) {
+    // Perform your email validation here
+    // You can use regular expressions or any other method
+    // to validate the email format
+    // For simplicity, this example checks if the email contains '@' symbol
+    return email.contains('@');
+  }
+}
--- lib/scr/screens/registrationpage.dart
+++ lib/scr/screens/registrationpage.dart
@@ -1,10 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:restoapp/scr/screens/loginpage.dart';
 
-class registrationpage extends StatelessWidget {
+class registrationpage extends StatefulWidget {
   const registrationpage({Key? key}) : super(key: key);
 
   @override
+  _registrationpageState createState() => _registrationpageState();
+}
+class _registrationpageState extends State<registrationpage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _username;
+  String? _email;
+  String? _password;
+  String? _confirmPassword;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _username = value;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  }
+                  if (!isValidEmail(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please confirm your password';
+                  }
+                  if (_password != null && value != _password) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _confirmPassword = value;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    if (_password != _confirmPassword) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Error'),
+                            content: Text('Passwords do not match'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      // Perform registration logic here using _username, _email, _password, _confirmPassword
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => loginpage()),
+                      );
+                    }
+                  }
+                },
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool isValidEmail(String email) {
+    // Perform your email validation here
+    // You can use regular expressions or any other method
+    // to validate the email format
+    // For simplicity, this example checks if the email contains '@' symbol
+    return email.contains('@');
   }
 }
