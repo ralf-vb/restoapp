f5c1f00693acfca3069975e0fbfe42932b13c25f
Intern-BDerequito <intern-bderequito@FDSAP-Admins-MacBook-Air.local>
2023-05-17 16:50:27 +0800

    images added

--- lib/scr/screens/appetizer.dart
+++ lib/scr/screens/appetizer.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class appetizer extends StatelessWidget {
   const appetizer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 300.0),
+        child: CarouselSlider(
+          options: CarouselOptions(
+            height: 200, // Adjust the height as needed
+            aspectRatio: 3 / 2, // Adjust the aspect ratio as needed
+            viewportFraction: 0.8, // Adjust the fraction of the viewport you want to show
+            initialPage: 0, // Set the initial page
+            enableInfiniteScroll: true, // Set to false if you don't want infinite scrolling
+            autoPlay: true, // Set to true for automatic sliding
+            autoPlayInterval: Duration(seconds: 3), // Adjust the interval between slides
+            autoPlayAnimationDuration: Duration(milliseconds: 800), // Adjust the animation duration
+            autoPlayCurve: Curves.fastOutSlowIn, // Adjust the animation curve
+            enlargeCenterPage: true, // Set to true if you want the center item to be larger
+          ),
+          items: [
+            // Replace the Placeholder widgets with your own images
+            Image.asset(
+              'assets/images/appetizer1.jpeg',
+              height: 300, // Adjust the height of the image
+              width: 400, // Adjust the width of the image
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'assets/images/appetizer2.jpeg',
+              height: 300, // Adjust the height of the image
+              width: 400, // Adjust the width of the image
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'assets/images/appetizer3.jpeg',
+              height: 300, // Adjust the height of the image
+              width: 400, // Adjust the width of the image
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/scr/screens/drinks.dart
+++ lib/scr/screens/drinks.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class drinks extends StatelessWidget {
-  const drinks({Key? key}) : super(key: key);
+  drinks({Key? key}) : super(key: key);
+
+  final List<Map<String, dynamic>> drinksList = [
+    {
+      'imagePath': 'assets/images/drinks1.jpeg',
+      'name': 'Drink 1',
+      'price': '\$5.99',
+    },
+    {
+      'imagePath': 'assets/images/drinks2.jpeg',
+      'name': 'Drink 2',
+      'price': '\$4.99',
+    },
+    {
+      'imagePath': 'assets/images/drinks3.jpeg',
+      'name': 'Drink 3',
+      'price': '\$6.99',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 300.0),
+        child: CarouselSlider(
+          options: CarouselOptions(
+            height: 200,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.8,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+          ),
+          items: drinksList.map((drink) {
+            return Column(
+              children: [
+                Image.asset(
+                  drink['imagePath'],
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  drink['name'],
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  drink['price'],
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
